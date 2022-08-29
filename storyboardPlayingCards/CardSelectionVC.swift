d9d578a9ff2948d162c7114bb78f21ca17eee5c3
ProdigyX6217 <dominic.holder.15@cityas.org>
2022-08-28 23:30:03 -0400

    Connect UI to Code & Create Rules View Controller

--- storyboardPlayingCards/CardSelectionVC.swift
+++ storyboardPlayingCards/CardSelectionVC.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class CardSelectionVC: UIViewController {
 
+    @IBOutlet var cardImageView: UIImageView!
+    @IBOutlet var buttons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for button in buttons {
+            button.layer.cornerRadius = 8
+        }
     }
 
-
+    @IBAction func pauseButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func restartButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func rulesButtonTapped(_ sender: UIButton) {
+    }
 }
 
